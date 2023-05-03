026d11c5453cfb21df0098dc054ed901dd0c7fbf
Filippe Braga <filippeffx@hotmail.com>
2023-05-03 20:38:13 -0300

    profile design

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,90 +1,109 @@
-// import React, { useState, useEffect } from 'react';
-// // import { useHistory } from 'react-router-dom';
-// import { FiPower, FiTrash2 } from 'react-icons/fi';
-// import Image from 'next/image';
+'use client'
 
-// // import api from '../../services/api';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import Image from 'next/image';
 
-// import './styles.css';
-// import Link from 'next/link';
+import api from '../../api/services/axiosClient';
 
-// export default function Profile() {
-//   const [incidents, setIncidents] = useState([]);
+import './styles.css';
+import Link from 'next/link';
+
+type Incident = {
+  id: number;
+  title: string;
+  description: string;
+  value: number;
+  ong_id: string;
+};
+
+
+export default function Profile() {
+  const [incidents, setIncidents] = useState<Incident[]>([]);
   
-//   // const history = useHistory();
+  const router = useRouter();
 
-//   const ongId = localStorage.getItem('ongId');
-//   const ongName = localStorage.getItem('ongName');
+  const ongId = localStorage.getItem('ongId');
+  const ongName = localStorage.getItem('ongName');
 
-//   useEffect(() => {
-//     api.get('profile', {
-//       headers: {
-//         Authorization: ongId
-//       }
-//     }).then(response => {
-//       setIncidents(response.data)
-//     })
-//   }, [ongId]);
+  useEffect(() => {
+    api.get('profile', {
+      headers: {
+        Authorization: ongId
+      }
+    }).then(response => {
+      setIncidents(response.data)
+    })
+  }, [ongId]);
 
-//   async function handleDeleteIncident(id) {
-//     try {
-//       await api.delete(`incidents/${id}`, {
-//         headers: {
-//           Authorization: ongId,
-//         }
-//       });
+  async function handleDeleteIncident(id: number) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId,
+        }
+      });
 
-//       // Fire Code = Font Ligatures
-//       setIncidents(incidents.filter(incidents => incidents.id !== id));
-//     } catch (err) {
-//       alert('Erro ao deletar caso, tente novamente.');
-//     }
-//   }
+      // Fire Code = Font Ligatures
+      setIncidents(incidents.filter(incidents => incidents.id !== id));
+    } catch (err) {
+      alert('Erro ao deletar caso, tente novamente.');
+    }
+  }
 
-//   function handleLogout() {
-//     localStorage.clear();
+  function handleLogout() {
+    localStorage.clear();
     
-//     // history.push('/');
-//   }
+    router.push('/');
+  }
 
-//   return (
-//     <div className="profile-container">
-//       <header>
-//         <Image src={'/logo.svg'} alt="Be The Hero" />
-//         <span>Bem vinda, {ongName}</span>
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 my-8">
+      <header className="flex items-center">
+        <Image 
+          className="h-24"
+          src={'/logo.svg'} 
+          alt="Be The Hero" 
+          width={100}
+          height={100}
+        />
+        <span className="text-xl ml-10" >Bem vinda, {ongName}</span>
 
-//         <Link className="button" href="/incidents/new">Cadastrar novo caso</Link>
-//         <button onClick={handleLogout} type="button">
-//           <FiPower size={18} color="#E02041" />
-//         </button>
-//       </header>
+        <Link className="w-full h-22 mt-4 py-4 bg-red-500 hover:bg-red-700 rounded-lg text-white items-center font-bold text-md" href="/incidents/new">Cadastrar novo caso</Link>
+        <button className="h-20 w-24 rounded-sm border-2 border-slate-500 bg-transparent ml-6 hover:border-gray-700 " onClick={handleLogout} type="button">
+          <FiPower size={18} color="#E02041" />
+        </button>
+      </header>
 
-//       <h1>Casos cadastrados</h1>
+      <h1>Casos cadastrados</h1>
 
-//       <ul>
-//         {incidents.map(incident => (
-//           <li key={incident.id}>
-//             <strong>CASO:</strong>
-//             <p>{incident.title}</p>
+      <div>
+        <ul>
+          {Array.isArray(incidents) && incidents.map(incident => (
+            <li key={incident.id}>
+              <strong>CASO:</strong>
+              <p>{incident.title}</p>
 
-//             <strong>DESCRIÇÃO:</strong>
-//             <p>{incident.description}</p>
+              <strong>DESCRIÇÃO:</strong>
+              <p>{incident.description}</p>
 
-//             <strong>Valor:</strong>
-//             <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
-            
-//             {/* in onClick I need the parameter to push the id  */}
-//             {/* but when I push the parameter I executed the function */}
-//             {/* to this thing not happen I just use the arrow function to create a new one */}
-//             {/* this way I'm passing to the onClick a function not a return of a function */}
-//             <button onClick={() => handleDeleteIncident(incident.id)} type="button">
-//               <FiTrash2 size={20} color="#A8A8B3" />
-//             </button>
-//           </li>
-//         ))}        
-//       </ul>
-//     </div>
-//   )
-// }
+              <strong>Valor:</strong>
+              <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
+              
+              {/* in onClick I need the parameter to push the id  */}
+              {/* but when I push the parameter I executed the function */}
+              {/* to this thing not happen I just use the arrow function to create a new one */}
+              {/* this way I'm passing to the onClick a function not a return of a function */}
+              <button onClick={() => handleDeleteIncident(incident.id)} type="button">
+                <FiTrash2 size={20} color="#A8A8B3" />
+              </button>
+            </li>
+          ))}        
+        </ul>
+      </div>      
+    </div>
+  )
+}
 
 
