9c2394e70b05454a09685a1cc5f9d3bf897774ea
Filippe Braga <filippeffx@hotmail.com>
2023-05-09 22:44:57 -0300

    form newincident

--- src/app/dashboard/new-incident/page.tsx
+++ src/app/dashboard/new-incident/page.tsx
@@ -53,32 +53,53 @@ export default function NewIncident() {
           Não tenho cadastro
         </Link> */}
       </header>
-      <section>    
+      <section >    
         <Link className="back-link" href="/profile">
           <FiArrowLeft size="16" color="#E02041"/>
           Voltar para home
         </Link>
       </section>
 
-      <form onSubmit={handleNewIncident}>
-        <input 
-          placeholder="Título do caso" 
-          value={title}
-          onChange={e => setTitle(e.target.value)}
-        />
-        <textarea 
-          placeholder="Descrição" 
-          value={description}
-          onChange={e => setDescription(e.target.value)}
-        />
-        <input 
-          placeholder="Valor em reais" 
-          value={value}
-          onChange={e => setValue(e.target.value)}
-        />
+      <section className="max-w-3xl w-full justify-center">
+        <form className="border-2 rounded-md border-gray-300 p-4 space-y-4" onSubmit={handleNewIncident}>
+          
+          <div className="">
+            <label htmlFor="title" className="text-lg font-medium text-gray-500">Título do Caso</label>
+            <input
+              className="mt-2 focus:ring-red-500 focus:border-red-500"
+              name="title"
+              type="text" 
+              placeholder="Título do caso" 
+              value={title}
+              onChange={e => setTitle(e.target.value)}              
+            />
+          </div> 
 
-        <button className="button" type="submit">Cadastrar</button>
-      </form>
+          <div className="">
+            <label htmlFor="description" className="text-lg font-medium text-gray-500">Descrição</label>
+            <textarea
+              className="mt-2 focus:ring-red-500 focus:border-red-500"
+              name="description"
+              placeholder="Descrição" 
+              value={description}
+              onChange={e => setDescription(e.target.value)}              
+            />
+          </div> 
+
+          <div className="">
+            <label htmlFor="value" className="text-lg font-medium text-gray-500">Valor em Reais</label>
+            <textarea
+              className="mt-2 focus:ring-red-500 focus:border-red-500"
+              name="value"
+              placeholder="Valor em reais" 
+              value={value}
+              onChange={e => setDescription(e.target.value)}              
+            />
+          </div> 
+
+          <button className="mt-8 relative w-full flex justify-center py-3 px-4 border border-transparent text-xl font-medium rounded-md text-white bg-red-500 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" type="submit">Cadastrar</button>
+        </form>
+      </section>      
     </div>
   );
 }
