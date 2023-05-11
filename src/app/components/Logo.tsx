90a16b059e7530377232c0fcee3ca951ba903ce0
Filippe Braga <filippeffx@hotmail.com>
2023-05-10 21:05:17 -0300

    logon logo reusable

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -2,16 +2,19 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div className="lg:col-span-2 justify-self-center">
-      <Image 
-        className="h-auto w-auto"
-        src={'/logo.svg'}                
-        alt="Be The Hero"
-        width={250}
-        height={106}
-        quality={70}
-        priority
-      />
-    </div>
+    <>
+      <div className="lg:col-span-2 justify-self-center">
+        <Image 
+          className=""
+          src={'/logo.svg'}                
+          alt="Be The Hero"
+          width={250 * 0.7}
+          height={106 * 0.7}
+          quality={70}
+          priority
+        />
+      </div>
+    </>
+    
   )  
 }
--- src/app/dashboard/logon/page.tsx
+++ src/app/dashboard/logon/page.tsx
@@ -1,13 +1,14 @@
 'use client'
 
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import { FiLogIn } from 'react-icons/fi';
 
 import api from '../../api/services/axiosClient';
 
-import Image from 'next/image';
-import Link from 'next/link';
+import Logo from '@/app/components/Logo';
 
 export default function Logon() {
   const [id, setId] = useState('');
@@ -32,44 +33,36 @@ export default function Logon() {
 
   return (
     <>
-      <div className="h-screen grid bg-gray-50 items-center justify-center px-4 sm:px-4 lg:px-6" >
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <header className="py-1">
-            <Image 
-              className="mx-auto h-14 w-auto"
-              src={'/logo.svg'} 
-              width={100} 
-              height={100} 
-              alt="Be The Hero"
-            />
-          </header>          
-          <form onSubmit={handleLogin}>
-            <h1 className="mt-4 py-2 text-center text-3xl">
-              Faça seu login
-            </h1>
-            <input 
-              className="mt-4"
-              placeholder="Sua ID" 
-              value={id}
-              onChange={e => setId(e.target.value)}
-            />
-            <button className="w-full h-22 mt-4 py-4 bg-red-500 hover:bg-red-700 rounded-lg text-white items-center font-bold text-md transition-opacity duration-300" type="submit">Entrar</button>
-            <div className="grid grid-cols-2 gap-2 mt-4">
-              <Link className="flex text-md items-center text-gray-900 hover:text-gray-500 font-medium" href="/register">
-                <FiLogIn className="mr-2 text-xl text-red-500" />
-                Não tenho cadastro
-              </Link>
+      <div className="min-h-screen flex flex-col items-center px-4 sm:px-12 lg:px-24" >
+        <header className="flex justify-center my-10">
+          <Logo />
+        </header>          
+        <form onSubmit={handleLogin}>
+          <h1 className="mt-4 py-2 text-center text-3xl">
+            Faça seu login
+          </h1>
+          <input 
+            className="mt-4"
+            placeholder="Sua ID" 
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
+          <button className="w-full h-22 mt-4 py-4 bg-red-500 hover:bg-red-700 rounded-lg text-white items-center font-bold text-md transition-opacity duration-300" type="submit">Entrar</button>
+          <div className="grid grid-cols-2 gap-2 mt-4">
+            <Link className="flex text-md items-center text-gray-900 hover:text-gray-500 font-medium" href="/register">
+              <FiLogIn className="mr-2 text-xl text-red-500" />
+              Não tenho cadastro
+            </Link>
 
-              {/* <div className="text-md items-center text-gray-900 hover:text-gray-500">
-                <a href="#" className="font-medium">
-                  Esqueci o password
-                </a>
-              </div> */}
+            {/* <div className="text-md items-center text-gray-900 hover:text-gray-500">
+              <a href="#" className="font-medium">
+                Esqueci o password
+              </a>
+            </div> */}
 
-            </div>
-            
-          </form>
-        </div>
+          </div>
+          
+        </form>
         <Image
           src={'/heroes.png'} 
           alt="Heroes"
@@ -79,6 +72,7 @@ export default function Logon() {
           priority
           />
       </div>
+      
     </>
   );
 }
