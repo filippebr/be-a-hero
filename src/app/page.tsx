a02d9bbe56a336f47656469f49ee29f252b1d476
Filippe Braga <filippeffx@hotmail.com>
2023-05-16 17:34:13 -0300

    map incidents using prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,15 @@ const inter = Inter({ subsets: ['latin'] })
 export default async function Home() {
   const response = await fetch("https://api.github.com/users/filippebr");
   const incidents = await prisma.incident.findMany();
+  
+  const data = incidents.map((incident) => {
+    return {
+      id: incident.id,
+      title: incident.title,
+      description: incident.description,
+      value: incident.value
+    }
+  })
 
   const user = await response.json();
 
@@ -15,9 +24,9 @@ export default async function Home() {
       <pre>
         {JSON.stringify(user.url, null, 2)}        
       </pre>
-      <pre>
-        {JSON.stringify(incidents[0], null, 2)}
-      </pre>
+      <ul>
+        {data.map(incident => <li key={incident.id}>{incident.title}</li>)}
+      </ul>
 
     </div>
   )
